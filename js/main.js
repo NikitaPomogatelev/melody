ea527f090d6fcf8d86c6017e91d18900f0210659
Никита Помогателев <pomogatelev@brick.dev>
2021-08-20 23:57:13 +0500

    Этажи + анимация + подсветка этажей

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(function () {
-    let currentFloor = 2; // переменная текущего этажа
+    let currentFloor = 7; // переменная текущего этажа
     const floorPath = $('.home-image path') // каждый отдельный этаж в svg
     const counterUp = $('.counter-arrow-up');
     const counterDown = $('.counter-arrow-down');
+    const modal = $('.modal');
+    const viewFlatsBtn = $('.view-flats'); 
 
     // функция при наведении машью на этаж
     floorPath.on('mouseover', function ()  {
@@ -11,8 +13,10 @@ $(function () {
         $('.counter').text(currentFloor) // записываем занечение этажа в счетчик справа
     });
 
+    viewFlatsBtn.on('click', toggleModal);
+
     counterUp.on('click', function () {
-        if(currentFloor < 18) {
+        if(currentFloor < floorPath.length + 1) {
             currentFloor++;
             usCurrentFloor = currentFloor.toLocaleString('en-Us', {minimumIntegerDigits: 2, useGrouping: false}); // форматирование 2 -> 02
             $('.counter').text(usCurrentFloor);
@@ -27,6 +31,18 @@ $(function () {
             $('.counter').text(usCurrentFloor);
             floorPath.removeClass('current-floor')
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
+        }
+    });
+    
+    function toggleModal() {
+        modal.toggleClass('is-open');
+    }
+
+    floorPath.on('click', toggleModal);
+    modal.on('click', function ({target}) {
+        console.log(target)
+        if (target.classList.contains('modal-close') || target.closest('.modal-close')) {
+            toggleModal();
         }
     });
 });
