18f10e1d7ba305590402531a6220cc6406287b4a
Никита Помогателев <pomogatelev@brick.dev>
2021-09-04 08:22:24 +0500

    Подсветка квартир

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ $(function () {
     const counterUp = $('.counter-arrow-up');
     const counterDown = $('.counter-arrow-down');
     const modal = $('.modal');
-    const viewFlatsBtn = $('.view-flats'); 
+    const viewFlatsBtn = $('.view-flats');
+    let currentFlat = 1; // текущая квартира
+    const flatsPath = $('.flats path'); // квартиры в svg
+    const flatsPathItem = $(".flat-item .flat-link")
 
     // функция при наведении машью на этаж
     floorPath.on('mouseover', function ()  {
@@ -12,6 +15,22 @@ $(function () {
         currentFloor = $(this).attr('data-floor'); // получаем и записываем класс текущего этажа
         $('.counter').text(currentFloor) // записываем занечение этажа в счетчик справа
     });
+
+    flatsPathItem.on('mouseover', function () {
+        currentFlat = $(this).attr("data-item"); // записываем текущее значение в переменную с квартирами
+        console.log(currentFlat);
+        flatsPath.removeClass("current-flats"); // удаляем класс квартир
+        flatsPathItem.removeClass("current-flats-item"); // удаляем класс характеристик квартиры
+        $(`[data-flats=${currentFlat}]`).toggleClass("current-flats"); // добавляем класс квартиры
+        $(`[data-item=${currentFlat}]`).toggleClass("current-flats-item"); // добавляем класс характеристик квартиры
+    })
+    flatsPath.on('mouseover', function () {
+        currentFlats = $(this).attr("data-flats"); // записываем текущее значение в переменную с квартирами
+        flatsPath.removeClass("current-flats"); // удаляем класс квартир
+        flatsPathItem.removeClass("current-flats-item"); // удаляем класс характеристик квартиры
+        $(`[data-flats=${currentFlats}]`).toggleClass("current-flats"); // добавляем класс квартиры
+        $(`[data-item=${currentFlats}]`).toggleClass("current-flats-item"); // добавляем класс характеристик квартиры
+    })
 
     viewFlatsBtn.on('click', toggleModal);
 
@@ -45,4 +64,6 @@ $(function () {
             toggleModal();
         }
     });
+
+    
 });
